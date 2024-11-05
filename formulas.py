6329d4cd2da93047b7aa48a1d26b60a2d9c8d6e7
Kacperux1 <kacper34505@o2.pl>
2024-11-05 21:30:07 +0100

    iimplementation of the pearson coeff and linear regression, wrong data

--- formulas.py
+++ formulas.py
@@ -1,21 +1,31 @@
 from math import sqrt
 from statistics import variance
 
+
 def arithmetic_average(values):
     sum = 0
     for value in values:
         sum += value
     return sum / len(values)
 
-def variation(values):
+
+
+def variance(values):
     temp = 0
-    ar_ave = arithmetic_average(values)
+    avg = arithmetic_average(values)
     for value in values:
-        temp += (value-ar_ave)**2
-    return temp / len(values)
+        temp += (value - avg) ** 2
+    return temp / (len(values) -1)
+
 
 def standard_deviation(values):
-    return sqrt(variation(values))
+    return sqrt(variance(values))
+
+def covariance(x_values, y_values):
+    result = 0
+    for i in range(len(x_values)):
+        result += (x_values[i] - arithmetic_average(x_values))*(y_values[i] - arithmetic_average(y_values))
+    return result / (len(x_values)-1)
 
 
 def find_max(values):
@@ -39,7 +49,7 @@ def median(values):
     if len(values) % 2 == 0:
         return (values[round(len(values) / 2) - 1] + values[round(len(values) / 2)]) / 2
     else:
-        return values[(round(len(values) / 2))-1]
+        return values[(round(len(values) / 2)) - 1]
 
 
 def Q1(values):
@@ -58,3 +68,20 @@ def Q3(values):
         return (values[int(index)] + values[int(index) + 1]) / 2
     else:
         return values[index]
+
+
+def linear_regression(x_values, y_values):
+    result = {"a": 0, "b": 0}
+
+    result["a"] = covariance(x_values, y_values) / variance(x_values)
+    result["b"] = arithmetic_average(y_values) - result["a"] * arithmetic_average(x_values)
+    return result
+
+
+
+
+def pearson_linear_corelation(x_values, y_values):
+    return covariance(x_values, y_values) / (standard_deviation(x_values)*standard_deviation(y_values))
+
+
+
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import formulas
 import matplotlib.pyplot as plt
+
+
 class Iris:
     def __init__(self, sepal_length, sepal_width, petal_length, petal_width, species):
         self.sepal_length = float(sepal_length)
@@ -34,8 +36,8 @@ with open("data1.csv") as data:
         iris = Iris(sepal_length, sepal_width, petal_length, petal_width, species)
         irises.append(iris)
 
-#for iris in irises:
-    #print(iris)
+# for iris in irises:
+# print(iris)
 
 species_count = {"setosa": 0, "versicolor": 0, "virginica": 0}
 
@@ -51,43 +53,53 @@ sepals_wid = []
 petals_len = []
 petals_wid = []
 
-setosa_sepals_len = []
-verbosa_sepals_len = []
-virginica_sepals_len = []
-setosa_sepals_wid = []
-verbosa_sepals_wid = []
-virginica_sepals_wid = []
-setosa_petals_len = []
-verbosa_petals_len = []
-virginica_petals_len = []
-setosa_petals_wid = []
-verbosa_petals_wid = []
-virginica_petals_wid = []
-
-
 for iris in irises:
     sepals_len.append(iris.sepal_length)
     sepals_wid.append(iris.sepal_width)
     petals_len.append(iris.petal_length)
     petals_wid.append(iris.sepal_width)
 
-
-
+init_sepals_len = [sepals_len]
+init_sepals_wid = [sepals_wid]
+init_petals_len = [petals_len]
+init_petals_wid = [petals_len]
 
 print(formulas.find_min(sepals_len))
 print(formulas.Q1(sepals_len))
 print(formulas.Q3(sepals_len))
 
 table2Date = [
-    ["długosc dzialki kielicha: ", formulas.find_min(sepals_len), str(round(formulas.arithmetic_average(sepals_len), 2))+"(+-"+str(round(formulas.standard_deviation(sepals_len), 2))+")", str(formulas.median(sepals_len))+"("+str(formulas.Q1(sepals_len))+" - "+str(formulas.Q3(sepals_len))+")", formulas.find_max(sepals_len)],
-    ["szerokosc dzialki kielicha: ", formulas.find_min(sepals_wid), str(round(formulas.arithmetic_average(sepals_wid), 2))+"(+-"+str(round(formulas.standard_deviation(sepals_wid), 2))+")", str(formulas.median(sepals_wid))+"("+str(formulas.Q1(sepals_wid))+" - "+str(formulas.Q3(sepals_wid))+")", formulas.find_max(sepals_wid)],
-    ["długosc platka: ", formulas.find_min(petals_len), str(round(formulas.arithmetic_average(petals_len), 2))+"(+-"+str(round(formulas.standard_deviation(petals_len), 2))+")", str(formulas.median(petals_len))+"("+str(formulas.Q1(petals_len))+" - "+str(formulas.Q3(petals_len))+")", formulas.find_max(petals_len)],
-    ["szerokosc platka: ", formulas.find_min(petals_wid), str(round(formulas.arithmetic_average(petals_wid), 2))+"(+-"+str(round(formulas.standard_deviation(petals_wid), 2))+")", str(formulas.median(petals_wid))+"("+str(formulas.Q1(petals_wid))+" - "+str(formulas.Q3(petals_wid))+")", formulas.find_max(petals_wid)]
+    ["długosc dzialki kielicha: ", formulas.find_min(sepals_len),
+     str(round(formulas.arithmetic_average(sepals_len), 2)) + "(+-" + str(
+         round(formulas.standard_deviation(sepals_len), 2)) + ")",
+     str(formulas.median(sepals_len)) + "(" + str(formulas.Q1(sepals_len)) + " - " + str(formulas.Q3(sepals_len)) + ")",
+     formulas.find_max(sepals_len)],
+    ["szerokosc dzialki kielicha: ", formulas.find_min(sepals_wid),
+     str(round(formulas.arithmetic_average(sepals_wid), 2)) + "(+-" + str(
+         round(formulas.standard_deviation(sepals_wid), 2)) + ")",
+     str(formulas.median(sepals_wid)) + "(" + str(formulas.Q1(sepals_wid)) + " - " + str(formulas.Q3(sepals_wid)) + ")",
+     formulas.find_max(sepals_wid)],
+    ["długosc platka: ", formulas.find_min(petals_len),
+     str(round(formulas.arithmetic_average(petals_len), 2)) + "(+-" + str(
+         round(formulas.standard_deviation(petals_len), 2)) + ")",
+     str(formulas.median(petals_len)) + "(" + str(formulas.Q1(petals_len)) + " - " + str(formulas.Q3(petals_len)) + ")",
+     formulas.find_max(petals_len)],
+    ["szerokosc platka: ", formulas.find_min(petals_wid),
+     str(round(formulas.arithmetic_average(petals_wid), 2)) + "(+-" + str(
+         round(formulas.standard_deviation(petals_wid), 2)) + ")",
+     str(formulas.median(petals_wid)) + "(" + str(formulas.Q1(petals_wid)) + " - " + str(formulas.Q3(petals_wid)) + ")",
+     formulas.find_max(petals_wid)]
 ]
 
 print(table2Date)
 
-plt.hist(sepals_len, bins=np.arange(formulas.find_min(sepals_len), formulas.find_max(sepals_len), 0.5), alpha=0.5, label='Data 2', color='blue', edgecolor='black', align='mid')
+corelation1 = formulas.pearson_linear_corelation(sepals_len, sepals_wid)
+print("r = " + str(corelation1))
+regression1 = formulas.linear_regression(sepals_len, sepals_wid)
+print("y = " + str(regression1["a"]) + "x + " + str(regression1["b"]))
+
+plt.hist(sepals_len, bins=np.arange(formulas.find_min(sepals_len), formulas.find_max(sepals_len), 0.5), alpha=0.5,
+         label='Data 2', color='blue', edgecolor='black', align='mid')
 plt.title('Długość działki kielicha')
 plt.xlabel('Długość (cm)')
 plt.ylabel('Liczebność')
@@ -95,14 +107,14 @@ plt.show()
 
 plt.figure()
 sepals_len_by_species = [[iris.sepal_length for iris in irises if iris.species == species]
-                         for species in ["setosa","versicolor",  "virginica"]]
+                         for species in ["setosa", "versicolor", "virginica"]]
 plt.boxplot(sepals_len_by_species, vert=True, tick_labels=['Setosa', 'Versicolor', 'Virginica'])
 plt.ylabel("Długość (cm)")
 plt.xlabel("Gatunek")
 plt.show()
 
-
-plt.hist(sepals_wid, bins=np.arange(formulas.find_min(sepals_wid), formulas.find_max(sepals_wid), 0.5), alpha=0.5, label='Data 2', color='blue', edgecolor='black')
+plt.hist(sepals_wid, bins=np.arange(formulas.find_min(sepals_wid), formulas.find_max(sepals_wid), 0.5), alpha=0.5,
+         label='Data 2', color='blue', edgecolor='black')
 plt.title('Szerokość działki kielicha')
 plt.xlabel('Długość (cm)')
 plt.ylabel('Liczebność')
@@ -111,13 +123,14 @@ plt.show()
 
 plt.figure()
 sepals_wid_by_species = [[iris.sepal_width for iris in irises if iris.species == species]
-                         for species in ["setosa","versicolor", "virginica"]]
+                         for species in ["setosa", "versicolor", "virginica"]]
 plt.boxplot(sepals_wid_by_species, vert=True, tick_labels=['Setosa', 'Versicolor', 'Virginica'])
 plt.ylabel("Długość (cm)")
 plt.xlabel("Gatunek")
 plt.show()
 
-plt.hist(petals_len, bins=np.arange(formulas.find_min(petals_len), formulas.find_max(petals_len), 0.5), alpha=0.5, label='Data 2', color='blue', edgecolor='black')
+plt.hist(petals_len, bins=np.arange(formulas.find_min(petals_len), formulas.find_max(petals_len), 0.5), alpha=0.5,
+         label='Data 2', color='blue', edgecolor='black')
 plt.title('Długość płatka')
 plt.xlabel('Długość (cm)')
 plt.ylabel('Liczebność')
@@ -126,14 +139,15 @@ plt.show()
 
 plt.figure()
 petal_len_by_species = [[iris.petal_length for iris in irises if iris.species == species]
-                         for species in ["setosa", "versicolor", "virginica"]]
+                        for species in ["setosa", "versicolor", "virginica"]]
 plt.boxplot(petal_len_by_species, vert=True, tick_labels=['Setosa', 'Versicolor', 'Virginica'])
 plt.title("Długość działki kielicha według gatunku")
 plt.ylabel("Długość (cm)")
 plt.xlabel("Gatunek")
 plt.show()
 
-plt.hist(petals_wid, bins=np.arange(formulas.find_min(petals_wid), formulas.find_max(petals_wid), 0.5), alpha=0.5, label='Data 2', color='blue', edgecolor='black')
+plt.hist(petals_wid, bins=np.arange(formulas.find_min(petals_wid), formulas.find_max(petals_wid), 0.5), alpha=0.5,
+         label='Data 2', color='blue', edgecolor='black')
 plt.title('Szerokość płatka')
 plt.xlabel('Długość (cm)')
 plt.ylabel('Liczebność')
@@ -142,8 +156,22 @@ plt.show()
 
 plt.figure()
 petal_wid_by_species = [[iris.petal_width for iris in irises if iris.species == species]
-                         for species in ["setosa", "versicolor", "virginica"]]
+                        for species in ["setosa", "versicolor", "virginica"]]
 plt.boxplot(petal_wid_by_species, vert=True, tick_labels=['Setosa', 'Versicolor', 'Virginica'])
 plt.ylabel("Długość (cm)")
 plt.xlabel("Gatunek")
+plt.show()
+
+
+
+corelation1 = formulas.pearson_linear_corelation(init_sepals_len, init_sepals_wid)
+print("r = " + str(corelation1))
+regression1 = formulas.linear_regression(init_sepals_len, init_sepals_wid)
+print("y = " + str(regression1["a"]) + "x + " + str(regression1["b"]))
+
+
+plt.scatter(sepals_len, sepals_wid, color='blue')
+plt.plot(sepals_len, regression1["a"] * np.array(sepals_len) + regression1["b"], color='red')
+plt.xlabel('X')
+plt.ylabel('Y')
 plt.show()
