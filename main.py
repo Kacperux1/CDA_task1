8b88e588dd86ca1c7a2e2ffba5222665cabc9a82
Kacperux1 <kacper34505@o2.pl>
2024-11-05 18:25:11 +0100

    box plots, not fully correct but present

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Iris:
         if species == '0':
             self.species = "setosa"
         elif species == '1':
-            self.species = "verbosa"
+            self.species = "versicolor"
         else:
             self.species = "virginica"
 
@@ -37,7 +37,7 @@ with open("data1.csv") as data:
 #for iris in irises:
     #print(iris)
 
-species_count = {"setosa": 0, "verbosa": 0, "virginica": 0}
+species_count = {"setosa": 0, "versicolor": 0, "virginica": 0}
 
 for iris in irises:
     for key in species_count:
@@ -50,11 +50,29 @@ sepals_len = []
 sepals_wid = []
 petals_len = []
 petals_wid = []
+
+setosa_sepals_len = []
+verbosa_sepals_len = []
+virginica_sepals_len = []
+setosa_sepals_wid = []
+verbosa_sepals_wid = []
+virginica_sepals_wid = []
+setosa_petals_len = []
+verbosa_petals_len = []
+virginica_petals_len = []
+setosa_petals_wid = []
+verbosa_petals_wid = []
+virginica_petals_wid = []
+
+
 for iris in irises:
     sepals_len.append(iris.sepal_length)
     sepals_wid.append(iris.sepal_width)
     petals_len.append(iris.petal_length)
     petals_wid.append(iris.sepal_width)
+
+
+
 
 print(formulas.find_min(sepals_len))
 print(formulas.Q1(sepals_len))
@@ -70,18 +88,33 @@ table2Date = [
 print(table2Date)
 
 plt.hist(sepals_len, bins=np.arange(formulas.find_min(sepals_len), formulas.find_max(sepals_len), 0.5), alpha=0.5, label='Data 2', color='blue', edgecolor='black', align='mid')
-
 plt.title('Długość działki kielicha')
 plt.xlabel('Długość (cm)')
 plt.ylabel('Liczebność')
-
 plt.show()
+
+plt.figure()
+sepals_len_by_species = [[iris.sepal_length for iris in irises if iris.species == species]
+                         for species in ["setosa","versicolor",  "virginica"]]
+plt.boxplot(sepals_len_by_species, vert=True, tick_labels=['Setosa', 'Versicolor', 'Virginica'])
+plt.ylabel("Długość (cm)")
+plt.xlabel("Gatunek")
+plt.show()
+
 
 plt.hist(sepals_wid, bins=np.arange(formulas.find_min(sepals_wid), formulas.find_max(sepals_wid), 0.5), alpha=0.5, label='Data 2', color='blue', edgecolor='black')
 plt.title('Szerokość działki kielicha')
 plt.xlabel('Długość (cm)')
 plt.ylabel('Liczebność')
 
+plt.show()
+
+plt.figure()
+sepals_wid_by_species = [[iris.sepal_width for iris in irises if iris.species == species]
+                         for species in ["setosa","versicolor", "virginica"]]
+plt.boxplot(sepals_wid_by_species, vert=True, tick_labels=['Setosa', 'Versicolor', 'Virginica'])
+plt.ylabel("Długość (cm)")
+plt.xlabel("Gatunek")
 plt.show()
 
 plt.hist(petals_len, bins=np.arange(formulas.find_min(petals_len), formulas.find_max(petals_len), 0.5), alpha=0.5, label='Data 2', color='blue', edgecolor='black')
@@ -91,9 +124,26 @@ plt.ylabel('Liczebność')
 
 plt.show()
 
+plt.figure()
+petal_len_by_species = [[iris.petal_length for iris in irises if iris.species == species]
+                         for species in ["setosa", "versicolor", "virginica"]]
+plt.boxplot(petal_len_by_species, vert=True, tick_labels=['Setosa', 'Versicolor', 'Virginica'])
+plt.title("Długość działki kielicha według gatunku")
+plt.ylabel("Długość (cm)")
+plt.xlabel("Gatunek")
+plt.show()
+
 plt.hist(petals_wid, bins=np.arange(formulas.find_min(petals_wid), formulas.find_max(petals_wid), 0.5), alpha=0.5, label='Data 2', color='blue', edgecolor='black')
 plt.title('Szerokość płatka')
 plt.xlabel('Długość (cm)')
 plt.ylabel('Liczebność')
 
+plt.show()
+
+plt.figure()
+petal_wid_by_species = [[iris.petal_width for iris in irises if iris.species == species]
+                         for species in ["setosa", "versicolor", "virginica"]]
+plt.boxplot(petal_wid_by_species, vert=True, tick_labels=['Setosa', 'Versicolor', 'Virginica'])
+plt.ylabel("Długość (cm)")
+plt.xlabel("Gatunek")
 plt.show()
