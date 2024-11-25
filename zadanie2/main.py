b477f5716c7cde14b11cd80284eb8f4f929537af
Jędrzej <251482@edu.p.lodz.pl>
2024-11-25 20:07:47 +0100

    wczytywanie danych

--- zadanie2/main.py
+++ zadanie2/main.py
@@ -1,1 +1,40 @@
-print("Hello World!")
+import csv
+
+# deklaracja klasy Iris, ktora jest wykorzystywana do wczytania pliku oraz do wyliczania wartosci w punkcie 1
+class Iris:
+    def __init__(self, sepal_length, sepal_width, petal_length, petal_width):
+        self.sepal_length = float(sepal_length)
+        self.sepal_width = float(sepal_width)
+        self.petal_length = float(petal_length)
+        self.petal_width = float(petal_width)
+
+
+irises = []
+# wczytanie z pliku, z separacja
+with open("data2.csv") as data:
+    reader = csv.reader(data)
+    for line in reader:
+        sepal_length = line[0]
+        sepal_width = line[1]
+        petal_length = line[2]
+        petal_width = line[3]
+        iris = Iris(sepal_length, sepal_width, petal_length, petal_width)
+        irises.append(iris)
+
+# deklaracja list, ktore beda zawierały poszczegolne wartosci mierzone
+sepals_len = []
+sepals_wid = []
+petals_len = []
+petals_wid = []
+
+# wczytanie wartosci do list
+for iris in irises:
+    sepals_len.append(iris.sepal_length)
+    sepals_wid.append(iris.sepal_width)
+    petals_len.append(iris.petal_length)
+    petals_wid.append(iris.petal_width)
+
+print(sepals_len)
+print(sepals_wid)
+print(petals_len)
+print(petals_wid)
